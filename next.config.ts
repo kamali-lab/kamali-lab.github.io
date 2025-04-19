c9ec2b7620693bcd162b0ba0f534171df5002d6b
kamali-lab <kaali@mun.ca>
2025-04-18 22:17:35 -0230

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: 'export',
+  output: 'export', // <--- Added for static export
   typescript: {
     ignoreBuildErrors: true,
   },
@@ -10,10 +10,7 @@ const nextConfig: NextConfig = {
     ignoreDuringBuilds: true,
   },
   experimental: {
-    // this will prevent any 3rd party library to be able to use `eval`
-    // and prevent angular from running
-    unsafeFunctionEval: false,
-  },
+    },
 };
 
 export default nextConfig;
